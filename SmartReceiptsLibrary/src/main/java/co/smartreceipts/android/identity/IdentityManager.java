e397d571beb5d31ec59cd3fbffaa15c4203cd821
Will Baumann <wbaumann@github.com>
2016-04-25 15:49:56 -0400

    Added a stub login request to the identity manager

--- SmartReceiptsLibrary/src/main/java/co/smartreceipts/android/identity/IdentityManager.java
+++ SmartReceiptsLibrary/src/main/java/co/smartreceipts/android/identity/IdentityManager.java
@@ -5,6 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import co.smartreceipts.android.apis.hosts.ServiceManager;
+import co.smartreceipts.android.apis.login.LoginPayload;
+import co.smartreceipts.android.apis.login.LoginResponse;
+import co.smartreceipts.android.apis.login.LoginService;
+import co.smartreceipts.android.apis.login.SmartReceiptsUserLogin;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class IdentityManager {
 
@@ -34,5 +41,33 @@ public class IdentityManager {
     @Nullable
     public Token getToken() {
         return mIdentityStore.getToken();
+    }
+
+    public void logIn(@NonNull final SmartReceiptsUserLogin login, @NonNull final LoginCallback loginCallback) {
+        final LoginService loginService = mServiceManager.getService(LoginService.class);
+        final LoginPayload request = new LoginPayload(login);
+
+        loginService.logIn(request).enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                if (response.isSuccessful()) {
+                    final LoginResponse body = response.body();
+                    if (body.getToken() != null && login.getEmail() != null) {
+                        mIdentityStore.setEmailAddress(login.getEmail());
+                        mIdentityStore.setToken(body.getToken());
+                        loginCallback.onSuccess();
+                    } else {
+                        loginCallback.onFailure();
+                    }
+                } else {
+                    loginCallback.onFailure();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                loginCallback.onFailure();
+            }
+        });
     }
 }
